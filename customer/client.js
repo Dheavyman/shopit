7de0a8bd1f82234690aad90aa89cdbbec7cb788d
Justin Nebo <nebojustin@gmail.com>
2020-09-30 02:18:44 +0100

    add grpc server url to environment

--- customer/client.js
+++ customer/client.js
@@ -5,6 +5,6 @@ const protoLoader = require('@grpc/proto-loader')
 const PROTO_PATH = path.join(__dirname, '..', 'protos', 'orders.proto')
 const packageDefinition = protoLoader.loadSync(PROTO_PATH)
 const orders = grpc.loadPackageDefinition(packageDefinition).orders
-const client = new orders.OrderService('0.0.0.0:8052', grpc.credentials.createInsecure())
+const client = new orders.OrderService(process.env.RPC_SERVER_URL, grpc.credentials.createInsecure())
 
 module.exports = client
